2cccb3413c07b9449ca53aeb2afd9dc714c08d94
Mojammel Hossen <142174470+MGIC4799@users.noreply.github.com>
2024-02-11 21:27:53 +0600

    Add files via upload

--- SnakeGame.java
+++ SnakeGame.java
@@ -1,3 +1,58 @@
+<<<<<<< HEAD
+
+package com.mycompany.practise;
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+public class SnakeGame extends JPanel{
+    private class Tile{
+        int a;
+        int b;
+        
+        // tile means each house
+        Tile(int a,int b){
+            this.a = a;
+            this.b = b;
+        }
+    }
+    int hight;
+    int width;
+    int tilesize = 25;
+    Tile food;
+    Tile head;
+    
+    SnakeGame(int hight,int width){
+        this.hight = hight;
+        this.width = width;
+        
+        
+        setPreferredSize(new Dimension(this.width,this.hight));
+        setBackground(Color.ORANGE);
+        
+        head = new Tile(5,5);
+
+	food = new Tile(10,10);
+    }
+    
+    public void paintComponent(Graphics gp){
+        super.paintComponent(gp);
+        draw(gp);
+    }
+    
+    public void draw(Graphics gp){
+        for(int i=0;i<width/tilesize;i++){
+            gp.drawLine(i*tilesize, 0, i*tilesize, hight);
+            gp.drawLine(0, i*tilesize,width, i*tilesize);
+        }
+        // it is for food
+        gp.setColor(Color.blue);
+        gp.fillRect(food.a * tilesize, food.b * tilesize, tilesize, tilesize);
+        // it is for snake
+        gp.setColor(Color.red);
+        gp.fillRect(head.a * tilesize, head.b * tilesize, tilesize, tilesize);
+    }
+}
+=======
 
 package com.mycompany.practise;
 import java.awt.*;
@@ -43,3 +98,4 @@ public class SnakeGame extends JPanel{
         gp.fillRect(own.a, own.b, scordsize, scordsize);
     }
 }
+>>>>>>> 65b567f73dc56d60f59d55588f41063d19351926
