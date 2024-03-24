6837ff746067c6e70f587eef9d21f4b97f71ea37
sojun15 <sojun2115@cseku.ac.bd>
2024-03-24 23:47:45 +0600

    large methods divide into small method

--- Speed.java
+++ Speed.java
@@ -6,15 +6,31 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 public class Speed{
     Speed(int hight, int width){
-    JFrame frame = new JFrame("Spreed");
-            frame.getContentPane().setBackground(Color.green);
-            frame.setVisible(true);
-            frame.setSize(width, hight);
-            frame.setLocationRelativeTo(null);
-            frame.setResizable(false);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);           
+    JFrame frame = new JFrame("Speed");
+            int y=100,add=50;
+            Frame(frame,hight,width);
+            
             JButton bton3=new JButton("High");
-            bton3.setBounds(100,100,150,30);
+            High(y,bton3,hight,width,frame);
+
+            JButton bton4=new JButton("Medium");
+            Medium(y+=add,bton4,hight,width,frame);
+                        
+            JButton bton5=new JButton("Low");
+            Low(y+=add,bton5,hight,width,frame);
+    }
+
+    void Frame(JFrame frame,int hight,int width){
+        frame.getContentPane().setBackground(Color.green);
+        frame.setVisible(true);
+        frame.setSize(width, hight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+    void High(int y,JButton bton3,int hight,int width,JFrame frame){
+            bton3.setBounds(100,y,150,30);
             bton3.setBackground(Color.yellow);
             bton3.addActionListener(new ActionListener(){  
             public void actionPerformed(ActionEvent e){ 
@@ -31,10 +47,11 @@ public class Speed{
         });
             frame.add(bton3);
             frame.setSize(400,400);  
-            frame.setLayout(null);frame.setVisible(true); 
-            
-            JButton bton4=new JButton("Medium");
-            bton4.setBounds(100,150,150,30);
+            frame.setLayout(null);frame.setVisible(true);
+    }
+    
+    void Medium(int y,JButton bton4,int hight,int width,JFrame frame){
+            bton4.setBounds(100,y,150,30);
             bton4.setBackground(Color.yellow);
             bton4.addActionListener(new ActionListener(){  
             public void actionPerformed(ActionEvent e){ 
@@ -52,9 +69,10 @@ public class Speed{
             frame.add(bton4);
             frame.setSize(400,400);  
             frame.setLayout(null);frame.setVisible(true);
-            
-            JButton bton5=new JButton("Low");
-            bton5.setBounds(100,200,150,30);
+    }
+    
+    void Low(int y,JButton bton5,int hight,int width,JFrame frame){
+            bton5.setBounds(100,y,150,30);
             bton5.setBackground(Color.yellow);
             bton5.addActionListener(new ActionListener(){  
             public void actionPerformed(ActionEvent e){ 
